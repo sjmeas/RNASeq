1deeeb1f3bafb026c15b754bdfaddf4013b4a71b
sjmeas <meass2@vcu.edu>
2020-06-24 11:25:43 -0400

    Edit variable

--- scripts/00_genome_sort.sh
+++ scripts/00_genome_sort.sh
@@ -10,10 +10,10 @@ mkdir -p $DIROUT
 GENOMETYPE=hg38.fa
 
 #Create order of chromosomes, create chr_ids.txt
-echo -e "10 \n11 \n12 \n13 \n14 \n15 \n16 \n17 \n18 \n19 \n1 \n20 \n21 \n22 \n2 \n3 \n4 \n5 \n6 \n7 \n8 \n9 \nM \nX \nY" >> $DIROUR/chr_ids.txt
+echo -e "10 \n11 \n12 \n13 \n14 \n15 \n16 \n17 \n18 \n19 \n1 \n20 \n21 \n22 \n2 \n3 \n4 \n5 \n6 \n7 \n8 \n9 \nM \nX \nY" >> $DIROUT/chr_ids.txt
 
 #Sort genome file based on chr_ids.txt
-samtools faidx $DIRIN/$GENOME $(cat chr_ids.txt) > #DIROUT/$GENOME
+samtools faidx $DIRIN/$GENOMETYPE $(cat chr_ids.txt) > $DIROUT/$GENOMETYPE
 
 #Gathering size of chromosomes, filtering out alt, random and Un
 fetchChromSizes hg38 | grep -v -E -- "random|alt|Un" > $DIROUT/hg38.list
