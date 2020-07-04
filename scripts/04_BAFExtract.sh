3249cc0a8efe28ad4c4d48db1732f4e28509287c
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2020-07-04 14:17:30 -0400

    Version tested on cluster

--- scripts/04_BAFExtract.sh
+++ scripts/04_BAFExtract.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+#PBS -S /bin/bash
+#PBS -V
+#PBS -l nodes=1:ppn=1
+#PBS -M mdozmorov@vcu.edu
+#PBS -m abe
+#PBS -N BAFExtrac
+#PBS -j oe
+# PBS -o /path/to/stderr-stdout/output
+
+cd $PBS_O_WORKDIR
+
 #Genome directory
 GENOMEDIR=genome_dir
 
@@ -7,6 +19,7 @@ GENOMETYPE=hg38.fa
 #Output directory
 PILEUPDIR=genome_fasta_pileup
 
+mkdir -p $PILEUPDIR
 
 #BAFEXtract Pre-processing to create genome_fasta_pileup_dir
 BAFExtract -preprocess_FASTA $GENOMEDIR/$GENOMETYPE $PILEUPDIR
@@ -23,6 +36,6 @@ for file in $(find $DIRIN/ -type f -name "*Aligned.out.bam")
 do
 	bam_file=$file;
 	output_baf_file=$DIROUT"/"`basename $file _001_trimmed.fq.gzAligned.out.bam`".snp";
-	samtools view $bam_file | BAFExtract -generate_compressed_pileup_per_SAM stdin $GENOMDIR/hg38.list $DIROUT 50 0;
+	samtools view $bam_file | BAFExtract -generate_compressed_pileup_per_SAM stdin $GENOMEDIR/hg38.list $DIROUT 50 0;
 	BAFExtract -get_SNVs_per_pileup $GENOMEDIR/hg38.list $DIROUT $PILEUPDIR 20 4 0.1 $output_baf_file;
 done
