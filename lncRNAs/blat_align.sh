4799ce4ba6e8fc67e140371490e85b52c643cfbe
sjmeas <meass2@vcu.edu>
2020-07-05 12:00:32 -0400

    Add blat processing locally

---
+++ lncRNAs/blat_align.sh
@@ -0,0 +1,8 @@
+#genome file that queries are matched to
+GENOME=~/Projects/RNASeq/genome_dir/hg38.fa
+
+#list of query files in fasta format
+ls . | grep "myseq" > sequence.list 
+
+#run blat
+blat ~/Projects/RNASeq/genome_dir/hg38.fa sequence.list blat_results.psl
