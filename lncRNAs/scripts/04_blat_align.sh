606bcf0922341e4cdbf0f8385bb4f971bbafe86d
sjmeas <meass2@vcu.edu>
2020-07-09 17:20:54 -0400

    Add preprocessing of genome data

--- lncRNAs/scripts/04_blat_align.sh
+++ lncRNAs/scripts/04_blat_align.sh
@@ -21,7 +21,12 @@ FILEOUT2=$DIROUT/blat_results_scored
 # Download and unzip the target genome
 wget http://hgdownload.cse.ucsc.edu/goldenpath/hg38/bigZips/hg38.fa.gz
 gunzip hg38.fa.gz
-mv hg38.fa $genome_dir
+#Create order of chromosomes, create chr_ids.txt
+echo -e "chr10 \nchr11 \nchr12 \nchr13 \nchr14 \nchr15 \nchr16 \nchr17 \nchr18 \nchr19 \nchr1 \nchr20 \nchr21 \nchr22 \nchr2 \nchr3 \nchr4 \nchr5 \nchr6 \nchr7 \nchr8 \nchr9 \nchrM \nchrX \nchrY" >> $genome_dir/chr_ids.txt
+#Sort genome file based on chr_ids.txt (remove alt, Un, random)
+samtools faidx hg38.fa $(cat $genome_dir/chr_ids.txt) > $genome_dir/hg38.fa
+#mv hg38.fa $genome_dir
+
 
 #run blat
 blat -t=dna -q=dna $genome_dir/hg38.fa $FILEIN $FILEOUT
