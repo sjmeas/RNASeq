def9cb9ae652f6572e177e5cb97096e97d84f041
sjmeas <meass2@vcu.edu>
2020-07-08 21:06:40 -0400

    Update naming of scripts

--- lncRNAs/bowtie2_align.sh
+++
@@ -1,8 +0,0 @@
-#bowtie build index
-bowtie2-build -f --threads 7 ~/Projects/RNASeq/genome_dir/hg38.fa hg38
-
-#bowtie run alignment
-bowtie2 -f -x hg38 -U ~/Projects/lncRNA/01_bedtools/results.fasta -S ~/Projects/lncRNA/02_aligned/aligned
-
-#with multiple threads
-bowtie2 -f --threads 7 -x hg38 -U ~/Projects/lncRNA/01_bedtools/results.fasta -S ~/Projects/lncRNA/02_aligned/aligned
